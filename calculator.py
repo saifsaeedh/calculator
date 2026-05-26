803a2c0e1079eccf3e28590281cf68632098fc99
saifsaeedh <saifsaeedh@icloud.com>
2026-05-27 04:01:23 +0500

    feat(calculator): added logger labels

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-print("==CALCULATOR V1==")
+print("==CALCULATOR V1.1==")
+print("[INFO] Initializing Calculator")
 print()
 
 print("This is a basic calculator that can take two values and do one of the following operations")
@@ -8,17 +9,25 @@ print("1. Add")
 print("2. Subtract")
 print("3. Multiply")
 print("4. Divide")
+print("5. Exit")
 print()
 
 print("Which of these operations do you want to do?")
 op = input(str("Enter Here: "))
+print(f"[DEBUG] op = {op}")
 print()
 
 print("What is the first value?")
 num1 = int(input("Enter here: "))
+print(f"[DEBUG] num1 = {num1}")
 print()
+
 print("What is the second value? ")
 num2 = int(input("Enter here: "))
+if num2 == "0":
+    print("[ERROR] Cannot divide a number by ZERO")
+else:
+    print(f"[DEBUG] num2= {num2}")
 print()
 
 if op == "1":
@@ -33,5 +42,5 @@ elif op == "3":
 elif op == "4":
     result = num1 / num2
 
-
+print(f"[DEBUG] Result = {result}")
 print(f"Result: {result}")
